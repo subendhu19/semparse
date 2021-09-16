db9fe232de3d6ba609bc94943aee01f03fcc9711
subendhu19 <subendhu.iitm@gmail.com>
2021-09-16 10:58:59 -0400

    Initial commit to test gpt-neo generation

---
+++ src/finetune/gpt.py
@@ -0,0 +1,6 @@
+from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
+
+if __name__ == "__main__":
+    gen = pipeline("text-generation", model="EleutherAI/gpt-neo-2.7B")
+    text_out = gen("I want one pizza with", do_sample=True, min_length=20)
+    print("Generated: {}".format(text_out["generated_text"]))
