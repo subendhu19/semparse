b0af9675fa0da2e4dae4fabbf9ddb4bbbc914cec
subendhu19 <subendhu.iitm@gmail.com>
2021-09-16 11:05:43 -0400

    Test commit

--- src/finetune/gpt.py
+++ src/finetune/gpt.py
@@ -4,3 +4,5 @@ if __name__ == "__main__":
     gen = pipeline("text-generation", model="EleutherAI/gpt-neo-2.7B")
     text_out = gen("I want one pizza with", do_sample=True, min_length=20)
     print("Generated: {}".format(text_out["generated_text"]))
+
+
