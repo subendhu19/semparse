7239b8578d08c7498259f86306af03aa198d58bf
subendhu19 <subendhu.iitm@gmail.com>
2021-10-12 14:30:38 -0400

    Updates to GPT Neo model

--- src/incontext/gpt.py
+++ src/incontext/gpt.py
@@ -1,6 +1,72 @@
-from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
+from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer, AutoModel
+import argparse
+import torch
+
 
 if __name__ == "__main__":
-    gen = pipeline("text-generation", model="EleutherAI/gpt-neo-2.7B")
-    text_out = gen("I want one pizza with", do_sample=True, min_length=20)
-    print("Generated: {}".format(text_out["generated_text"]))
+    # gen = pipeline("text-generation", model="EleutherAI/gpt-neo-2.7B")
+    # text_out = gen("I want one pizza with", do_sample=True, min_length=20)
+    # print("Generated: {}".format(text_out["generated_text"]))
+
+    parser = argparse.ArgumentParser(description="GPT in-context for semantic parsing")
+
+    parser.add_argument('--data_prefix', type=str, default="/Users/subendhu/Documents/Amazon 2021"
+                                                           "/jt_data/overnight/basketball/original/basketball")
+    parser.add_argument('--gpu', action='store_true')
+    parser.add_argument('--out_folder', type=str, default="/Users/subendhu/Documents/Amazon 2021")
+    parser.add_argument('--constrained_dict_path', type=str)
+
+    args = parser.parse_args()
+
+    out_folder = args.out_folder
+
+    if args.gpu:
+        if not torch.cuda.is_available():
+            print('No GPU found. Quitting')
+            exit(1)
+        device = "cuda:{}".format(torch.cuda.current_device())
+    else:
+        device = "cpu"
+
+    # Data Processing
+    with open(args.data_prefix + '.train.utterances.txt') as inf:
+        train_utt = [l.strip() for l in inf.readlines()]
+
+    with open(args.data_prefix + '.train.canonical.txt') as inf:
+        train_can = [l.strip() for l in inf.readlines()]
+
+    train_data = zip(train_utt, train_can)
+
+    with open(args.data_prefix + '.test.utterances.txt') as inf:
+        test_utt = [l.strip() for l in inf.readlines()]
+
+    with open(args.data_prefix + '.test.canonical.txt') as inf:
+        test_can = [l.strip() for l in inf.readlines()]
+
+    test_data = zip(test_utt, test_can)
+
+    # Relevance scoring
+    tokenizer = AutoTokenizer.from_pretrained('EleutherAI/gpt-neo-1.3B')
+    model = AutoModel.from_pretrained('EleutherAI/gpt-neo-1.3B').to(device)
+
+    train_embeddings = []
+    test_embeddings = []
+
+    for (utt, can) in train_data:
+        inputs = tokenizer(utt, return_tensors="pt").to(device)
+        outputs = model(**inputs)
+
+        train_embeddings.append(outputs.last_hidden_state)
+
+    for (utt, can) in test_data:
+        inputs = tokenizer(utt, return_tensors="pt").to(device)
+        outputs = model(**inputs)
+
+        test_embeddings.append(outputs.last_hidden_state)
+
+    train_embeddings = torch.stack(train_embeddings, dim=0)
+    test_embeddings = torch.stack(test_embeddings, dim=0)
+
+    relevance_scores = torch.cosine_similarity(train_embeddings, test_embeddings, dim=1)
+
+    print(relevance_scores.shape)
